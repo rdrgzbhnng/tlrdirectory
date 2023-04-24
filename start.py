8d7eceeb3634ad058a01ab5874385e60ba6fa2f2
Carlos Alberto <oiueei@disroot.org>
2023-04-24 15:10:54 +0200

    Add routes

--- start.py
+++ start.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask ( __name__ )
 
 @app.route ( "/" )
-def test ( ):
-  return "This is a test"
+def index( ):
+  return "Home page"
+
+@app.route('/results/<int:post_id>')
+def results(post_id):
+    return f'Post {post_id}'
