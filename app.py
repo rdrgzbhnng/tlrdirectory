f1cd197f88f14362f5f69d034116f343bb733aa4
Carlos Alberto <oiueei@disroot.org>
2023-05-02 15:36:29 +0200

    Add routes and templates

--- app.py
+++ app.py
@@ -7,6 +7,30 @@ app = Flask ( __name__ )
 def index(name=None):
   return render_template('home.html', name=name)
 
+@app.route ( "/companyprofile" )
+def companyprofile(name=None):
+  return render_template('companyprofile.html', name=name)
+
+@app.route ( "/warehouses" )
+def warehouses(name=None):
+  return render_template('warehouses.html', name=name)
+
+@app.route ( "/frbrcompanies" )
+def frbrcompanies(name=None):
+  return render_template('frbrcompanies.html', name=name)
+
+@app.route ( "/frbrwarehouses" )
+def frbrwarehouses(name=None):
+  return render_template('frbrwarehouses.html', name=name)
+
+@app.route ( "/spanishcontacts" )
+def spanishcontacts(name=None):
+  return render_template('spanishcontacts.html', name=name)
+
+@app.route ( "/frbrwarehousesspanishcontacts" )
+def frbrwarehousesspanishcontacts(name=None):
+  return render_template('frbrwarehousesspanishcontacts.html', name=name)
+
 @app.route('/results/<int:post_id>')
 def results(post_id):
     return f'Post {post_id}'
