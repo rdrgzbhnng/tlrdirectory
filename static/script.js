840dcea7be467899c18b86d908c3b306796c3b11
Carlos Alberto <oiueei@disroot.org>
2023-05-03 15:36:27 +0200

    Update templates

--- static/script.js
+++ static/script.js
@@ -24,14 +24,14 @@ cleanIt = function(st) {
 warehouseTexting = function(stlc) {
   getLIs(st);
   stlc = st.toLowerCase();
-  if (stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega")) {
-    resultDom.innerHTML = pressEnter + findWarehouses + wName33 + wName34 + wName35 + wName36 + wName37 + wName38 + wName39;
+  if (stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega") || stlc.includes("almacenes")) {
+    resultDom.innerHTML = pressEnter + findWarehouses + wName33 + wName34 + wName35 + wName36 + wName37 + wName38 + wName39 + moreInformation;
   } else if (stlc.includes("warehous") || stlc.includes("warehou")) {
-    resultDom.innerHTML = pressEnter + coFrWarehouseBordeauxName12;
+    resultDom.innerHTML = pressEnter + coFrWarehouseBordeauxName12 + moreInformation;
   } else if (stlc.includes("wareho") || stlc.includes("wareh")) {
-    resultDom.innerHTML = pressEnter + coFrWarehouseBordeauxName12 + coFrWarehomeName13;
+    resultDom.innerHTML = pressEnter + coFrWarehouseBordeauxName12 + coFrWarehomeName13 + moreInformation;
   } else if (stlc.includes("ware")) {
-    resultDom.innerHTML = pressEnter + coFrWareName11 + coFrWarehouseBordeauxName12 + coFrWarehomeName13;
+    resultDom.innerHTML = pressEnter + coFrWareName11 + coFrWarehouseBordeauxName12 + coFrWarehomeName13 + moreInformation;
   } 
 };
 
@@ -40,61 +40,61 @@ bordeauxTexting = function(st) {
   stlc = st.toLowerCase();
   if (stlc.includes("bordeaux") || stlc.includes("fr33") || stlc.includes("fr 33") || stlc.includes("fr-33") || stlc.includes("f33") || stlc.includes("f 33") || stlc.includes("f-33") || stlc.includes("33***") || stlc.includes("33000")) 
   {
-    resultDom.innerHTML = pressEnter + findCoFr33Bordeaux + coFrWarehouseBordeauxName12 + coBordeaux16 + coBordeaux17 + coBordeaux18 + coBordeaux19 + coFrBordeauxName14 + coBordeaux20;
+    resultDom.innerHTML = pressEnter + findCoFr33Bordeaux + coFrWarehouseBordeauxName12 + coBordeaux16 + coBordeaux17 + coBordeaux18 + coBordeaux19 + coFrBordeauxName14 + coBordeaux20 + moreInformation;
   } else if (stlc.includes("bordea")) {
-    resultDom.innerHTML = pressEnter + coFrBordeauxName14;
+    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + moreInformation;
   } else if (stlc.includes("borde")) {
-    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + coFrBordeName15;
+    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + coFrBordeName15 + moreInformation;
   } else if (stlc.includes("bord")) {
-    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + coFrBordeName15 + bordName26 + bordName25 + bordName27 + bordName28;
+    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + coFrBordeName15 + bordName26 + bordName25 + bordName27 + bordName28 + moreInformation;
   } else if (stlc.includes("bor")) {
-    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + bordName25 + bordName26 + bordName27 + bordName28 + bName29 + bName30;
+    resultDom.innerHTML = pressEnter + coFrBordeauxName14 + bordName25 + bordName26 + bordName27 + bordName28 + bName29 + bName30 + moreInformation;
   }
 };
 
 phoneTexting = function(st) {
   getLIs(st);
   if (st.includes("695892792")) {
-    resultDom.innerHTML = pressEnter + phFr05;
+    resultDom.innerHTML = pressEnter + phFr05 + moreInformation;
   } else if (st.includes("69589279")) {
-    resultDom.innerHTML = pressEnter + phFr05;
+    resultDom.innerHTML = pressEnter + phFr05 + moreInformation;
   } else if (st.includes("6958927")) {
-    resultDom.innerHTML = pressEnter + phFr05;
+    resultDom.innerHTML = pressEnter + phFr05 + moreInformation;
   } else if (st.includes("695892")) {
-    resultDom.innerHTML = pressEnter + phFr02 + phFr05;
+    resultDom.innerHTML = pressEnter + phFr02 + phFr05 + moreInformation;
   } else if (st.includes("69589")) {
-    resultDom.innerHTML = pressEnter + phFr02 + phFr03 + phFr05;
+    resultDom.innerHTML = pressEnter + phFr02 + phFr03 + phFr05 + moreInformation;
   } else if (st.includes("6958")) {
-    resultDom.innerHTML = pressEnter + phFr02 + phFr03 + phFr05 + docFr23;
+    resultDom.innerHTML = pressEnter + phFr02 + phFr03 + phFr05 + docFr23 + moreInformation;
   } else if (st.includes("695")) {
-    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + docFr04 + phFr03 + docFr22 + docFr23 + phFr24;
+    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + docFr04 + phFr03 + docFr22 + docFr23 + phFr24 + moreInformation;
   } else if (st.includes("69")) {
-    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + docFr04 + phFr03 + phBo21 + docFr22 + docFr23;
+    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + docFr04 + phFr03 + phBo21 + docFr22 + docFr23 + moreInformation;
   } else if (st.includes("6")) {
-    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + phFr03 + docFr04 + phFr05 + coVAT12phN6 + coVAT1phN6 ;
+    resultDom.innerHTML = pressEnter + phFr01 + phFr02 + phFr03 + docFr04 + phFr05 + coVAT12phN6 + coVAT1phN6 + moreInformation;
   } 
 };
 
 numbersTexting = function(st) {
   getLIs(st);
   if (st.includes("1234567890")) {
-    resultDom.innerHTML = pressEnter + vatFr09;
+    resultDom.innerHTML = pressEnter + vatFr09 + moreInformation;
   } else if (st.includes("123456789")) {
-    resultDom.innerHTML = pressEnter + vatFr09;
+    resultDom.innerHTML = pressEnter + vatFr09 + moreInformation;
   } else if (st.includes("1234567")) {
-    resultDom.innerHTML = pressEnter + vatFr09;
+    resultDom.innerHTML = pressEnter + vatFr09 + moreInformation;
   } else if (st.includes("123456")) {
-    resultDom.innerHTML = pressEnter + idFr08 + vatFr09;
+    resultDom.innerHTML = pressEnter + idFr08 + vatFr09 + moreInformation;
   } else if (st.includes("12345")) {
-    resultDom.innerHTML = pressEnter + idFr08 + vatFr09;
+    resultDom.innerHTML = pressEnter + idFr08 + vatFr09 + moreInformation;
   } else if (st.includes("1234")) {
-    resultDom.innerHTML = pressEnter + vatFr06 + idFr08 + vatFr09;
+    resultDom.innerHTML = pressEnter + vatFr06 + idFr08 + vatFr09 + moreInformation;
   } else if (st.includes("123")) {
-    resultDom.innerHTML = pressEnter + vatFr06 + idFr08 + vatFr09;
+    resultDom.innerHTML = pressEnter + vatFr06 + idFr08 + vatFr09 + moreInformation;
   } else if (st.includes("12")) {
-    resultDom.innerHTML = pressEnter + vatFr06 + vatFr07 + idFr08 + vatFr09 + vatFr10 + coVAT12phN6;
+    resultDom.innerHTML = pressEnter + vatFr06 + vatFr07 + idFr08 + vatFr09 + vatFr10 + coVAT12phN6 + moreInformation;
   } else if (st.includes("1")) {
-    resultDom.innerHTML = pressEnter + vatFr06 + vatFr07 + idFr08 + vatFr09 + vatFr10 + coVAT12phN6 + coVAT1phN6 ;
+    resultDom.innerHTML = pressEnter + vatFr06 + vatFr07 + idFr08 + vatFr09 + vatFr10 + coVAT12phN6 + coVAT1phN6 + moreInformation;
   }
 };
 
@@ -102,18 +102,18 @@ spanishTexting = function(st){
   getLIs(st);
   stlc = st.toLowerCase();
   if (stlc.includes("spanish") || stlc.includes("castellano") || stlc.includes("español")) {
-    resultDom.innerHTML = pressEnter + findCoSpanish + spanishCo40 + spanishCo41 + spanishCo42 + spanishCo43 + spanishCo44 + spanishCo45 + spanishCo46;
+    resultDom.innerHTML = pressEnter + findCoSpanish + spanishCo40 + spanishCo41 + spanishCo42 + spanishCo43 + spanishCo44 + spanishCo45 + spanishCo46 + moreInformation;
   } else if (stlc.includes("spanis") || stlc.includes("spani")) {
-    resultDom.innerHTML = pressEnter + findCoSpanish + spanishCo47 + spanishCo40 + spanishCo48 + spanishCo49 + spanishCo42 + spanishCo43 + spanishCo44;
+    resultDom.innerHTML = pressEnter + findCoSpanish + spanishCo47 + spanishCo40 + spanishCo48 + spanishCo49 + spanishCo42 + spanishCo43 + spanishCo44 + moreInformation;
   }
 };
 
 warehouseANDbordeaux = function(st) {
   getLIs(st);
   stlc = st.toLowerCase();
-  if ((stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega")) 
+  if ((stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega") || stlc.includes("almacenes")) 
   && (stlc.includes("bordeaux") || stlc.includes("fr33") || stlc.includes("fr 33") || stlc.includes("fr-33") || stlc.includes("f33") || stlc.includes("f 33") || stlc.includes("f-33"))) {
-    resultDom.innerHTML = pressEnter + findWarehousesInBordeaux + findCoFr33Bordeaux + coFrWarehouseBordeauxName12 + coFrBordeauxName14;
+    resultDom.innerHTML = pressEnter + findWarehousesInBordeaux + findCoFr33Bordeaux + coFrWarehouseBordeauxName12 + coFrBordeauxName14 + moreInformation;
   }
 };
 
@@ -122,16 +122,16 @@ bordeauxANDspanish = function(st) {
   stlc = st.toLowerCase();
   if ((stlc.includes("bordeaux") || stlc.includes("fr33") || stlc.includes("fr 33") || stlc.includes("fr-33") || stlc.includes("f33") || stlc.includes("f 33") || stlc.includes("f-33")) 
   && (stlc.includes("spanish") || stlc.includes("castellano") || stlc.includes("español"))) {
-    resultDom.innerHTML = pressEnter + findCoFr33Bordeaux +  findCoSpanish + coFrBordeauxName14 + coBordeaux18 + coBordeaux19;
+    resultDom.innerHTML = pressEnter + findCoFr33Bordeaux +  findCoSpanish + coFrBordeauxName14 + coBordeaux18 + coBordeaux19 + moreInformation;
   }
 };
 
 warehouseANDspanish = function(st) {
   getLIs(st);
   stlc = st.toLowerCase();
-  if ((stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega")) 
+  if ((stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega") || stlc.includes("almacenes")) 
   && (stlc.includes("spanish") || stlc.includes("castellano") || stlc.includes("español"))) {
-    resultDom.innerHTML = pressEnter + findWarehouses + findCoSpanish + wareSP50 + wareSP51 + wareSP52 + wareSP53 + wareSP54 + wareSP55;   
+    resultDom.innerHTML = pressEnter + findWarehouses + findCoSpanish + wareSP50 + wareSP51 + wareSP52 + wareSP53 + wareSP54 + wareSP55 + moreInformation;
   }
 };
 
@@ -139,9 +139,9 @@ warehouseANDbordeauxANDspanish = function(st) {
   getLIs(st);
   stlc = st.toLowerCase();
   if ((stlc.includes("bordeaux") || stlc.includes("fr33") || stlc.includes("fr 33") || stlc.includes("fr-33") || stlc.includes("f33") || stlc.includes("f 33") || stlc.includes("f-33")) 
-  && (stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega")) 
+  && (stlc.includes("warehouse") || stlc.includes("building") || stlc.includes("almacén") || stlc.includes("depósito") || stlc.includes("bodega") || stlc.includes("almacenes")) 
   && (stlc.includes("spanish") || stlc.includes("castellano") || stlc.includes("español"))) {
-    resultDom.innerHTML = pressEnter + findWarehousesInBordeaux + findCoFr33Bordeaux + findCoSpanish + warehouseBordeauxSp57; 
+    resultDom.innerHTML = pressEnter + findWarehousesInBordeaux + findCoFr33Bordeaux + findCoSpanish + warehouseBordeauxSp57 + moreInformation;
   }
 };
 
@@ -151,22 +151,23 @@ getLIs = function(st) {
   findWarehouses              = '<li class="main"><a href="warehouses"><svg><use xlink:href="#search"></svg>Find "<b>warehouses</b>"</a></li>';
   findCoSpanish               = '<li class="main"><a href="spanishcontacts"><svg><use xlink:href="#search"></svg>Find contacts speak "<b>Spanish</b>"</a></li>';
   findWarehousesInBordeaux    = '<li class="main"><a href="frbrwarehouses"><svg><use xlink:href="#search"></svg>Find "<b>warehouses</b> in "<b>Bordeaux</b>"</a></li>';
+  moreInformation             = '<li class="main"><a href="frbrwarehouses"><svg><use xlink:href="#search"></svg>Not the results you had expected? <b class="link">More information</b></a></li>';
   phFr01                      = '<li><a href="companyprofile"><svg><use xlink:href="#phone"></svg><b>FR 56380	SDV TRANSPORT SAS</b><span>VAT: FR8593955615	Id: 44321890</span></a></li>';
   phFr02                      = '<li><a href="companyprofile"><svg><use xlink:href="#phone"></svg><b>FR 77950	STTEFG INTERNATIONAL</b><span>VAT: FR6658280687	Id: 66453584</span></a></li>';
   phFr03                      = '<li><a href="companyprofile"><svg><use xlink:href="#phone"></svg><b>FR 91300	TPC COURCELLAS</b><span>VAT: FR1937521762	Id: 50001111</span></a></li>';
   docFr04                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 59390	ABC XPO FR CELL GROUP</b><span>VAT: FR69500233683	Id: 48420001</span></a></li>';
-  phFr05                      = '<li><a href="companyprofile"><svg><use xlink:href="#phone"></svg><b>FR 15000	100% FRET DISTRIB</b><span>VAT: FR1157515065	Id: 93955615</span></a></li>';
+  phFr05                      = '<li><a href="companyprofile"><svg><use xlink:href="#phone"></svg><b>FR 33100 MAX Export-Import</b><span>VAT: FR1234567890  Id: 97234867</span></a></li>';
   vatFr06                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 07370	BEAUGIER EXPRESS</b><span>VAT: FR12344321890	Id: 475903485</span></a></li>';
   vatFr07                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 93370	5G TRANSPORTS</b><span>VAT: FR12000001111	Id: 23465674</span></a></li>';
   idFr08                      = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 93210	BARBAR-YVES</b><span>VAT: FR52312316830	Id: 123456000</span></a></li>';
-  vatFr09                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 95200	ADELE MANAGEMENT</b><span>VAT: FR12345678900	Id: 57523435</span></a></li>';
+  vatFr09                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 33100  MAX Export-Import</b><span>VAT: FR1234567890  Id: 97234867</span></a></li>';
   vatFr10                     = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 28630	LE PRESSIS EUROTRANS</b><span>VAT: FR12999999999	Id: 157515065</span></a></li>';
   coVAT12phN6                 = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 75300	ORLEANS DES SABLONS</b><span>VAT: FR1256845346	Id: 235455645</span></a></li>';
   coVAT1phN6                  = '<li><a href="companyprofile"><svg><use xlink:href="#document"></svg><b>FR 15100	EU-BOURGOGNE-FORICHER</b><span>VAT: FR198553434	Id: 84452343</span></a></li>';
   coFrWareName11              = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 94150	BENNES WAREWAVE</b><span>VAT: FR1392191196	Id: 183751768</span></a></li>';
   coFrWarehouseBordeauxName12 = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33323	BORDEAUX-WAREHOUSES LOGISTIC</b><span>VAT: FR2362221082	Id: 34545654</span></a></li>';
   coFrWarehomeName13          = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33521	WAREHOME</b><span>VAT: FR9864606952	Id: 58280687</span></a></li>';
-  coFrBordeauxName14          = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33100	BORDEAUX NORD TRANSP</b><span>VAT: FR8542337668	Id: 97234867</span></a></li>';
+  coFrBordeauxName14          = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33100 MAX Export-Import</b><span>VAT: FR1234567890  Id: 97234867</span></a></li>';
   coFrBordeName15             = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33520	UNBORDERS</b><span>VAT: FR8748420001	Id: 45686766</span></a></li>';
   coBordeaux16                = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33010	CABOT-LOIRET</b><span>VAT: FR2482968234	Id: 12999999</span></a></li>';
   coBordeaux17                = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33140	CONDITIONNEMENT TAVAUX</b><span>VAT: FR5896987337	Id: 62221082</span></a></li>';
@@ -211,7 +212,7 @@ getLIs = function(st) {
   wareSP54                    = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33520	UNBORDERS</b><span>VAT: FR8748420001	Id: 45686766</span></a></li>';
   wareSP55                    = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 40010	CABOT-LOIRET</b><span>VAT: FR2482968234	Id: 12999999</span></a></li>';
   wareSP56                    = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33140	CONDITIONNEMENT TAVAUX</b><span>VAT: FR5896987337	Id: 62221082</span></a></li>';
-  warehouseBordeauxSp57       = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33100	BORDEAUX NORD TRANSP</b><span>VAT: FR8542337668	Id: 97234867</span></a></li>';
+  warehouseBordeauxSp57       = '<li><a href="companyprofile"><svg><use xlink:href="#company"></svg><b>FR 33100	MAX Export-Import</b><span>VAT: FR1234567890	Id: 97234867</span></a></li>';
 };
 
 clickToCleanIt = function() {
